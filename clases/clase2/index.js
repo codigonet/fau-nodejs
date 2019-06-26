f16017d2333351f9364e848bd912563093a1d9da
Cristian Acuña <cas@codigonet.net>
2019-06-26 17:00:36 -0400

    código usado en clase 2

---
+++ clases/clase2/index.js
@@ -0,0 +1,12 @@
+function Mensaje(){
+	var mensaje = "Hola";
+	console.log(mensaje + ", buenas noches");
+}
+
+var mensaje = "Antes del mensaje";
+console.log(mensaje);
+
+Mensaje();
+
+mensaje = 12345;
+console.log("Despues del mensaje... " + mensaje);
